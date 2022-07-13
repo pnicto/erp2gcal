da7730be6f152072bdd33a7e5f12e4d79ec1685e
Qwerty <thehulk489@gmail.com>
2022-07-13 12:00:00 +0530

    FInish getting data from erp

---
+++ erp.py
@@ -0,0 +1,22 @@
+# Selenium imports
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.edge.service import Service as EdgeService
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
+driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
+
+driver = webdriver.Edge()
+
+# Login logic
+driver.get("https://sis.erp.bits-pilani.ac.in/psc/sisprd/EMPLOYEE/SA/c/SA_LEARNER_SERVICES.SSS_STUDENT_CENTER.GBL?NavColl=true&ICAGTarget=start")
+
+WebDriverWait(driver, 50).until(
+    EC.presence_of_element_located((By.CLASS_NAME, 'PATRANSACTIONTITLE')))
+
+schedule = driver.execute_script("return document.querySelector('.PSLEVEL1GRIDWBO')")
+
+tableData = schedule.text
+
