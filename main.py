64595c2f35a6c9f19b5e450050c895907d2b58d9
Qwerty <thehulk489@gmail.com>
2022-07-14 19:14:17 +0530

    Add choose browser and colored output

--- main.py
+++ main.py
@@ -1,25 +1,72 @@
+# Selenium imports
+from lib2to3.pgen2 import driver
 from selenium import webdriver
+# Edge imports
 from selenium.webdriver.edge.service import Service as EdgeService
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from selenium.webdriver.edge.options import Options as edge_options
+# Chrome imports
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options as chrome_options
+# Firefox imports
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+# Brave imports
+from selenium.webdriver.chrome.service import Service as BraveService
+from webdriver_manager.core.utils import ChromeType
+# Opera imports
+from webdriver_manager.opera import OperaDriverManager
 
 import cms
 import courseParse
 import erp
 import erp2gcal
 
+# Colors from terminal output
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def select_preferred_browser():
+    print(f"\n{bcolors.OKCYAN}Choose a installed browser among the list:{bcolors.ENDC}\n")
+    print(f"\n{bcolors.OKCYAN}1. Edge\n2. Chrome\n3. Firefox\n4. Brave\n5. Opera{bcolors.ENDC}")
+
+    browser_choice = int(input(f"\n{bcolors.HEADER}Enter your choice(1-5):{bcolors.ENDC}\n"))
+    if browser_choice==1:
+        # Welp I tried turning the webdriver messages off let's see how well
+        options = edge_options()
+        options.add_argument("--log-level=OFF")
+        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
+    elif browser_choice==2:
+        options = chrome_options()
+        options.add_argument("--log-level=OFF")
+        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    elif browser_choice==3:
+        driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+    elif browser_choice==4:
+        driver = webdriver.Chrome(service=BraveService(ChromeDriverManager(chrome_type=ChromeType.BRAVE).install()))
+    elif browser_choice==5:
+        driver = webdriver.Opera(executable_path=OperaDriverManager().install())
+    else:
+        print(f'{bcolors.FAIL}Choose from the given options{bcolors.ENDC}')
+    return driver
+
 if __name__ == "__main__":
-    print("\n1. Unenrol from prev courses, Enrol in new courses, Create calendar events\n2. Unenrol from prev courses, Enrol in new courses\n3. Enrol\n4. Unenrol\n5. Create calendar events")
+    print(f"\n{bcolors.HEADER}1. Unenrol from prev courses, Enrol in new courses, Create calendar events\n2. Unenrol from prev courses, Enrol in new courses\n3. Enrol\n4. Unenrol\n5. Create calendar events{bcolors.ENDC}")
 
-    choice = int(input("\nEnter your choice(1-5):\n"))
-
-    # TODO: Give option to choose browser
-    # print("\nChoose a installed browser among the list:\n")
-    # print("\n1. Edge\n2. Chrome\n3. Firefox\n4. Brave\n5. Opera")
-
-    # browser_choice = int(input("\nEnter your choice(1-5):\n"))
-
-    driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
-
+    choice = int(input(f"\n{bcolors.HEADER}Enter your choice(1-5):{bcolors.ENDC}\n"))
+    
+    driver = select_preferred_browser()
     if choice == 1:
         try:
             # Unenrol, Enrol, Calendar events
@@ -58,7 +105,7 @@ if __name__ == "__main__":
                 service=gcal_service,
             )
         except Exception as err:
-            print(err)
+            print(f"{bcolors.FAIL}{err}{bcolors.ENDC}")
 
     elif choice == 2:
         try:
@@ -91,7 +138,7 @@ if __name__ == "__main__":
             )
 
         except Exception as err:
-            print(err)
+            print(f"{bcolors.FAIL}{err}{bcolors.ENDC}")
 
     elif choice == 3:
         try:
@@ -116,7 +163,7 @@ if __name__ == "__main__":
                 security_key=security_key,
             )
         except Exception as err:
-            print(err)
+            print(f"{bcolors.FAIL}{err}{bcolors.ENDC}")
 
     elif choice == 4:
         try:
@@ -137,7 +184,7 @@ if __name__ == "__main__":
                 cookie=cookie,
             )
         except Exception as err:
-            print(err)
+            print(f"{bcolors.FAIL}{err}{bcolors.ENDC}")
 
     elif choice == 5:
         try:
@@ -155,4 +202,4 @@ if __name__ == "__main__":
                 service=gcal_service,
             )
         except Exception as err:
-            print(err)
+            print(f"{bcolors.FAIL}{err}{bcolors.ENDC}")
