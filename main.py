851ed10044c90cd191453ce342a1e3cc6db5fbad
Qwerty <thehulk489@gmail.com>
2022-07-16 16:55:04 +0530

    Refactored to make it less repetitive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # Selenium imports
-from lib2to3.pgen2 import driver
 from selenium import webdriver
 # Edge imports
 from selenium.webdriver.edge.service import Service as EdgeService
@@ -36,8 +35,8 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 def select_preferred_browser():
-    print(f"\n{bcolors.OKCYAN}Choose a installed browser among the list:{bcolors.ENDC}\n")
-    print(f"\n{bcolors.OKCYAN}1. Edge\n2. Chrome\n3. Firefox\n4. Brave\n5. Opera{bcolors.ENDC}")
+    print(f"\n{bcolors.OKCYAN}Choose an installed browser among the list:{bcolors.ENDC}\n")
+    print(f"{bcolors.OKCYAN}1. Edge\n2. Chrome\n3. Firefox\n4. Brave\n5. Opera{bcolors.ENDC}")
 
     browser_choice = int(input(f"\n{bcolors.HEADER}Enter your choice(1-5):{bcolors.ENDC}\n"))
     if browser_choice==1:
@@ -62,23 +61,28 @@ def select_preferred_browser():
     return driver
 
 if __name__ == "__main__":
-    print(f"\n{bcolors.HEADER}1. Unenrol from prev courses, Enrol in new courses, Create calendar events\n2. Unenrol from prev courses, Enrol in new courses\n3. Enrol\n4. Unenrol\n5. Create calendar events{bcolors.ENDC}")
+    try:
+        driver = select_preferred_browser()
+        # CMS Login
+        cms.perform_login(driver)
+        # Get params
+        (
+            security_key,
+            session_key,
+            user_id,
+            cookie,
+        ) = cms.get_required_parameters_to_make_requests(driver)
+        # ERP Login
+        erp.navigation_and_login(driver)
+        erp_registered_courses = erp.get_schedule(driver)
+        # Create "Course" class instances
+        courses = courseParse.coursesGen(erp_registered_courses)
 
-    choice = int(input(f"\n{bcolors.HEADER}Enter your choice(1-5):{bcolors.ENDC}\n"))
+        print(f"\n{bcolors.HEADER}1. Unenrol from prev courses, Enrol in new courses, Create calendar events\n2. Unenrol from prev courses, Enrol in new courses\n3. Enrol\n4. Unenrol\n5. Create calendar events{bcolors.ENDC}")
 
-    driver = select_preferred_browser()
-    if choice == 1:
-        try:
-            # Unenrol, Enrol, Calendar events
-            # CMS Login
-            cms.perform_login(driver)
-            # Get params
-            (
-                security_key,
-                session_key,
-                user_id,
-                cookie,
-            ) = cms.get_required_parameters_to_make_requests(driver)
+        choice = int(input(f"\n{bcolors.HEADER}Enter your choice(1-5):{bcolors.ENDC}\n"))
+
+        if choice == 1:
             # Unenrol
             cms.unenrol_from_all_courses(
                 user_id=user_id,
@@ -86,116 +90,13 @@ if __name__ == "__main__":
                 security_key=security_key,
                 cookie=cookie,
             )
-            # ERP Login
-            erp.navigation_and_login(driver)
-            erp_registered_courses = erp.get_schedule(driver)
-            # Create "Course" class instances
-            courses = courseParse.coursesGen(erp_registered_courses)
             # Enrol
             cms.enrol_all_registered_courses(
                 courses=courses,
                 cookie=cookie,
                 security_key=security_key,
             )
-            # Google calenda auth
-            gcal_service = erp2gcal.google_auth()
-            # Creating events
-            erp2gcal.create_gcal_events(
-                courses=courses,
-                service=gcal_service,
-            )
-            # Remove the side effect
-            erp2gcal.clean_the_unnecessary_events(service=gcal_service)
-        except Exception as err:
-            print(f"{bcolors.FAIL}{err}{bcolors.ENDC}")
-
-    elif choice == 2:
-        try:
-            # Unenrol, Enrol
-            cms.perform_login(driver)
-            # Get params
-            (
-                security_key,
-                session_key,
-                user_id,
-                cookie,
-            ) = cms.get_required_parameters_to_make_requests(driver)
-            # Unenrol
-            cms.unenrol_from_all_courses(
-                user_id=user_id,
-                session_key=session_key,
-                security_key=security_key,
-                cookie=cookie,
-            )
-            # ERP Login
-            erp.navigation_and_login(driver)
-            erp_registered_courses = erp.get_schedule(driver)
-            # Create "Course" class instances
-            courses = courseParse.coursesGen(erp_registered_courses)
-            # Enrol
-            cms.enrol_all_registered_courses(
-                courses=courses,
-                cookie=cookie,
-                security_key=security_key,
-            )
-
-        except Exception as err:
-            print(f"{bcolors.FAIL}{err}{bcolors.ENDC}")
-
-    elif choice == 3:
-        try:
-            # Enrol
-            cms.perform_login(driver)
-            # Get params
-            (
-                security_key,
-                session_key,
-                user_id,
-                cookie,
-            ) = cms.get_required_parameters_to_make_requests(driver)
-            # ERP Login
-            erp.navigation_and_login(driver)
-            erp_registered_courses = erp.get_schedule(driver)
-            # Create "Course" class instances
-            courses = courseParse.coursesGen(erp_registered_courses)
-            # Enrol
-            cms.enrol_all_registered_courses(
-                courses=courses,
-                cookie=cookie,
-                security_key=security_key,
-            )
-        except Exception as err:
-            print(f"{bcolors.FAIL}{err}{bcolors.ENDC}")
-
-    elif choice == 4:
-        try:
-            # Unenrol
-            cms.perform_login(driver)
-            # Get params
-            (
-                security_key,
-                session_key,
-                user_id,
-                cookie,
-            ) = cms.get_required_parameters_to_make_requests(driver)
-            # Unenrol
-            cms.unenrol_from_all_courses(
-                user_id=user_id,
-                session_key=session_key,
-                security_key=security_key,
-                cookie=cookie,
-            )
-        except Exception as err:
-            print(f"{bcolors.FAIL}{err}{bcolors.ENDC}")
-
-    elif choice == 5:
-        try:
             # Calendar events
-            # ERP Login
-            erp.navigation_and_login(driver)
-            erp_registered_courses = erp.get_schedule(driver)
-            # Create "Course" class instances
-            courses = courseParse.coursesGen(erp_registered_courses)
             # Google calenda auth
             gcal_service = erp2gcal.google_auth()
             # Creating events
@@ -205,5 +106,51 @@ if __name__ == "__main__":
             )
             # Remove the side effect
             erp2gcal.clean_the_unnecessary_events(service=gcal_service)
-        except Exception as err:
-            print(f"{bcolors.FAIL}{err}{bcolors.ENDC}")
+
+        elif choice == 2:
+            # Unenrol
+            cms.unenrol_from_all_courses(
+                user_id=user_id,
+                session_key=session_key,
+                security_key=security_key,
+                cookie=cookie,
+            )
+            # Enrol
+            cms.enrol_all_registered_courses(
+                courses=courses,
+                cookie=cookie,
+                security_key=security_key,
+            )
+
+
+        elif choice == 3:
+            # Enrol
+            cms.enrol_all_registered_courses(
+                courses=courses,
+                cookie=cookie,
+                security_key=security_key,
+            )
+
+        elif choice == 4:
+            # Unenrol
+            cms.unenrol_from_all_courses(
+                user_id=user_id,
+                session_key=session_key,
+                security_key=security_key,
+                cookie=cookie,
+            )
+
+
+        elif choice == 5:
+            # Calendar events
+            # Google calenda auth
+            gcal_service = erp2gcal.google_auth()
+            # Creating events
+            erp2gcal.create_gcal_events(
+                courses=courses,
+                service=gcal_service,
+            )
+            # Remove the side effect
+            erp2gcal.clean_the_unnecessary_events(service=gcal_service)
+    except Exception as err:
+        print(f"{bcolors.FAIL}{err}{bcolors.ENDC}")
