58f5914e953550c98425f094932316bbfd2a72c7
pnicto <thehulk489@gmail.com>
2022-08-30 00:39:06 +0530

    Fixed multiple timings for a course in parser

--- courseParse.py
+++ courseParse.py
@@ -1,4 +1,5 @@
 import datetime as dt
+import re
 
 # Do not try to understand :D
 
@@ -24,13 +25,20 @@ class Course:
 def coursesGen(erp_registered_courses):
     courses = []
     for i in range(0, len(erp_registered_courses), 4):
-        # Assign starting and ending time of a course to start,end in RFC3399 format using timeGen function
+        if i == len(erp_registered_courses):
+            break
+
+        # Debugging be like
+        # print("=========")
+        # print(i)
+        # print(len(erp_registered_courses))
+        # print("=========")
 
         # WORKSHOP PRAC FLAG
         WSFLAG = False
         if "ME F112-P2" == erp_registered_courses[i]:
             WSFLAG = True
-
+        # Assign starting and ending time of a course to start,end in RFC3399 format using timeGen function
         start, end = timeGen(
             erp_registered_courses[i + 2].split()[1],
             erp_registered_courses[i + 1].split()[0],
@@ -47,6 +55,32 @@ def coursesGen(erp_registered_courses):
                 end,
             )
         )
+
+        if (i + 4 < len(erp_registered_courses)) and (
+            not re.match(
+                r"^[A-Z]{2,4}\s[A-Z]\d{3}-[A-Z]\d", erp_registered_courses[i + 4]
+            )
+        ):
+            # Assign starting and ending time of a course to start,end in RFC3399 format using timeGen function
+            start, end = timeGen(
+                erp_registered_courses[i + 4].split()[1],
+                erp_registered_courses[i + 1].split()[0],
+                WSFLAG,
+            )
+
+            courses.append(
+                Course(
+                    erp_registered_courses[i],
+                    erp_registered_courses[i + 1].split()[0],
+                    erp_registered_courses[i + 5].split()[1],
+                    erp_registered_courses[i + 4].split()[0],
+                    start,
+                    end,
+                )
+            )
+            erp_registered_courses.remove(erp_registered_courses[i + 4])
+            erp_registered_courses.remove(erp_registered_courses[i + 4])
+
     return courses
 
 
