3c760da9687d0cd4071fea505478f08af2f3775b
pnicto <thehulk489@gmail.com>
2022-12-28 16:31:09 +0530

    Revert "Update imports"
    This reverts commit e0f7dd6902967dcbdbab51ebfb84f6beabfb5cd5.

--- src/erp2gcal.py
+++ src/erp2gcal.py
@@ -5,7 +5,7 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from utils.bcolors import bcolors
+from utils import bcolors
 
 
 class Erp2Gcal:
--- src/main.py
+++ src/main.py
@@ -19,7 +19,7 @@ from cms import CmsActions
 from course_parse import CourseParser
 from erp import ErpActions
 from erp2gcal import Erp2Gcal
-from utils.bcolors import bcolors
+from utils import bcolors
 
 
 def select_preferred_browser():
