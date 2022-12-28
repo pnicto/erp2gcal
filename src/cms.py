d642bc0da407d38d515dc7d97ff8b5819bdc18e6
pnicto <thehulk489@gmail.com>
2022-12-28 16:31:08 +0530

    Revert "Change var names, add spaces, comments"
    This reverts commit 93ee172a97b5b4e2070fc545672b5e3a7f69356e.

--- src/cms.py
+++ src/cms.py
@@ -9,23 +9,25 @@ from utils.bcolors import bcolors
 
 
 class CmsActions:
-    def perform_cms_login(driver):
+    # Function which goes to login page
+    def perfrom_cms_login(driver):
         try:
+            # Go to CMS
             driver.get("https://cms.bits-hyderabad.ac.in/login/index.php")
-            # Finding the "Login with google button"
+            # Finding the login with google button
             login_button = driver.find_element(By.LINK_TEXT, "Google")
             login_button.click()
-            # Login action waiting for user to enter details to login and load the page
+            # Login action waiting for user to enter details to login
             WebDriverWait(driver, 100).until(
                 EC.presence_of_element_located((By.ID, "page-my-index"))
             )
         except Exception as err:
             print(f"{bcolors.FAIL}{err}{bcolors.ENDC}")
 
-    # Function which returns security key, session key, user id, cookie to make requests
+    # Function which returns security key, session key, user id to make requests
     def get_required_parameters_to_make_requests(driver):
         try:
-            # Go to preferences page to get security key and session key
+            # Preferences page to get security key and session key
             driver.get("https://cms.bits-hyderabad.ac.in/user/preferences.php")
             WebDriverWait(driver, 15).until(
                 EC.presence_of_element_located((By.LINK_TEXT, "Security keys"))
@@ -37,22 +39,24 @@ class CmsActions:
             security_key = driver.find_element(By.CSS_SELECTOR, ".cell.c0").text
             session_key = driver.current_url.split("=")[1]
 
-            # Goes to the edit profile menu to get user id
+            # Since firefox was having issue clicking that context menu this is a workaround for that
             driver.get("https://cms.bits-hyderabad.ac.in/user/preferences.php")
             WebDriverWait(driver, 15).until(
                 EC.presence_of_element_located((By.LINK_TEXT, "Edit profile"))
             )
+            # Goes to the edit profile menu to get user id
             driver.find_element(By.LINK_TEXT, "Edit profile").click()
             user_id = driver.current_url.split("&")[0].split("=")[1]
 
             # Session token from cookies
             moodle_session = driver.get_cookie("MoodleSession")["value"]
+            # Cookie
             cookie = {"MoodleSession": moodle_session}
-
             return (security_key, session_key, user_id, cookie)
         except Exception as err:
             print(f"{bcolors.FAIL}{err}{bcolors.ENDC}")
 
+    # Function which unenrolls from *all* courses
     def unenrol_from_all_courses(security_key, user_id, session_key, cookie):
         try:
             # Gets all enrolled courses and parses them
@@ -69,10 +73,8 @@ class CmsActions:
                 enrol_instance = requests.get(
                     f"https://cms.bits-hyderabad.ac.in/webservice/rest/server.php?wsfunction=core_enrol_get_course_enrolment_methods&moodlewsrestformat=json&wstoken={security_key}&courseid={course['id']}"
                 ).json()
-
                 # For all the enrolled courses get instance id
                 enrolId = enrol_instance[0]["id"]
-
                 # Make unenrol request
                 requests.get(
                     f"https://cms.bits-hyderabad.ac.in/enrol/self/unenrolself.php?confirm=1&enrolid={enrolId}&sesskey={session_key}",
@@ -85,8 +87,9 @@ class CmsActions:
         except Exception as err:
             print(f"{bcolors.FAIL}{err}{bcolors.ENDC}")
 
-    # Function which enrols into registered courses
-    def enrol_into_registered_courses(
+    # Function which enrolls you in *all* registered courses on ERP
+    # Please do not try to understand :D
+    def enrol_all_registered_courses(
         security_key,
         cookie,
         courses,
@@ -97,7 +100,7 @@ class CmsActions:
                 print(f"Searching for {course.name} in courses...")
                 course_search_term = " ".join(course.name.split("-"))
 
-                # Search for the course
+                # Normal course search
                 cms_api_search_results = requests.get(
                     f"https://cms.bits-hyderabad.ac.in/webservice/rest/server.php?wsfunction=core_course_search_courses&moodlewsrestformat=json&wstoken={security_key}&criterianame=search&criteriavalue={course_search_term}",
                     cookies=cookie,
@@ -168,7 +171,6 @@ class CmsActions:
         except Exception as err:
             print(f"{bcolors.FAIL}{err}{bcolors.ENDC}")
 
-    # Function which enrols into L/P sections of a course except for workshop practical
     def enrol_into_main_sections(
         security_key,
         cookie,
@@ -178,14 +180,11 @@ class CmsActions:
             print(f"{bcolors.HEADER}Enrolling into L/P sections\n{bcolors.ENDC}")
             for course in courses:
                 course_search_term = " ".join(course.name.split("-"))[:-1]
-
-                # Search only if the course is a practical or lecture
                 if (
                     course_search_term.endswith("P") or course_search_term.endswith("L")
                 ) and (course_search_term != "ME F111 P"):
-
-                    # Search for the course
                     print(f"\nSearching for {course_search_term}")
+
                     cms_api_search_results = requests.get(
                         f"https://cms.bits-hyderabad.ac.in/webservice/rest/server.php?wsfunction=core_course_search_courses&moodlewsrestformat=json&wstoken={security_key}&criterianame=search&criteriavalue={course_search_term}",
                         cookies=cookie,
@@ -215,9 +214,7 @@ class CmsActions:
                             + "Enter one of the above numbers to enrol into the corresponding course.\nType in 'n' and 'p' to navigate to the next and previous pages respectively.\nTo skip this course, type in 's'\n"
                             + bcolors.ENDC
                         )
-
                         user_choice = input()
-
                         if user_choice == "s":
                             break
                         else:
@@ -231,11 +228,11 @@ class CmsActions:
                                     course_id = cms_api_search_results["courses"][
                                         (user_choice - 1)
                                     ]["id"]
-                                    enrol_result = requests.get(
+                                    enrolRes = requests.get(
                                         f"https://cms.bits-hyderabad.ac.in/webservice/rest/server.php?wsfunction=enrol_self_enrol_user&moodlewsrestformat=json&wstoken={security_key}&courseid={course_id}",
                                         cookies=cookie,
                                     ).json()
-                                    if enrol_result["status"]:
+                                    if enrolRes["status"]:
                                         print(
                                             f"{bcolors.OKGREEN}Enrolled in course {cms_api_search_results['courses'][ (user_choice - 1)]['fullname']} successfully!{bcolors.ENDC}"
                                         )
