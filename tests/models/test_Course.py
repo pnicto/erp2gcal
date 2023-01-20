2d756df5671322d8379eaf3fa43c55a47e2a9c21
pnicto <ydkr789@gmail.com>
2023-01-20 12:12:42 +0530

    test: Add test for course start timings parser

--- tests/models/test_Course.py
+++ tests/models/test_Course.py
@@ -1,5 +1,13 @@
-from models.Course import Course
+import datetime as dt
+
 import pytest
+
+from models.Course import Course
+
+
+def create_date_time_isoformat_from_hour(hour):
+    TODAY = dt.datetime.now()
+    return dt.datetime(TODAY.year, TODAY.month, TODAY.day, hour).isoformat()
 
 
 class TestCourse:
@@ -17,3 +25,17 @@ class TestCourse:
     def test_raise_error_when_day_is_missing(self):
         with pytest.raises(ValueError):
             Course("", "LEC", "", "", "2:00PM")
+
+    @pytest.mark.parametrize(
+        "time,expected",
+        [
+            ("12:00AM", create_date_time_isoformat_from_hour(0)),
+            ("6:00AM", create_date_time_isoformat_from_hour(6)),
+            ("12:00PM", create_date_time_isoformat_from_hour(12)),
+            ("6:00PM", create_date_time_isoformat_from_hour(18)),
+            ("6:30PM", create_date_time_isoformat_from_hour(18)),
+        ],
+    )
+    def test_parse_start_timings(self, time, expected):
+        course = Course("", "LEC", "", "MoWeFr", time)
+        assert course.start == expected
