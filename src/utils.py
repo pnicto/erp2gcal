5f9b1a492e8c3a7c0a3c5f5734d47db66d06dd73
pnicto <ydkr789@gmail.com>
2023-04-03 11:56:25 +0530

    feat: Use regex completely for parser
    The previous implementation was very difficult to debug and very error prone. Hopefully this should do better.

--- src/utils.py
+++ src/utils.py
@@ -17,7 +17,6 @@ os.environ["WDM_LOCAL"] = "1"
 
 
 def initialize_driver_for_preferred_browser(browser_arg):
-
     try:
         if browser_arg == "chrome":
             driver = webdriver.Chrome(
@@ -44,38 +43,60 @@ def initialize_driver_for_preferred_browser(browser_arg):
         )
 
 
-def parse_string_to_courses(registered_course_str):
-    registered_course_str_list = registered_course_str.split("\n")[2:]
+def parse_string_to_courses(registered_course_str_list):
     parsed_courses = []
+    normal_class_pattern = r"[A-Z]{2,4}\s[FG]\d{3}-[LPT]\d+\n[A-Z]{3}\s\(\d{4}\)\n(Mo|Tu|We|Th|Fr|Sa|Su)+\s\b\d{1,2}:\d{2}[AP]M\s-\s\d{1,2}:\d{2}[AP]M\b\nRoom\s+TBA"
+    split_class_pattern = r"[A-Z]{2,4}\s[FG]\d{3}-[LPT]\d+\n[A-Z]{3}\s\(\d{4}\)\n(Mo|Tu|We|Th|Fr|Sa|Su)+\s\b\d{1,2}:\d{2}[AP]M\s-\s\d{1,2}:\d{2}[AP]M\b\nRoom\s+TBA\n(Mo|Tu|We|Th|Fr|Sa|Su)+\s\b\d{1,2}:\d{2}[AP]M\s-\s\d{1,2}:\d{2}[AP]M\b\nRoom\s+TBA"
 
-    for idx in range(0, len(registered_course_str_list), 4):
-        if idx == len(registered_course_str_list):
+    for idx in range(len(registered_course_str_list)):
+        if idx + 4 >= len(registered_course_str_list):
             break
 
-        parsed_course = Course(
-            registered_course_str_list[idx],
-            registered_course_str_list[idx + 1].split()[0],
-            registered_course_str_list[idx + 3].split()[1],
-            registered_course_str_list[idx + 2].split()[0],
-            registered_course_str_list[idx + 2].split()[1],
+        normal_class_str_to_match = "\n".join(
+            [
+                registered_course_str_list[idx],
+                registered_course_str_list[idx + 1],
+                registered_course_str_list[idx + 2],
+                registered_course_str_list[idx + 3],
+            ]
         )
 
-        parsed_courses.append(parsed_course)
+        normal_class_match = re.match(normal_class_pattern, normal_class_str_to_match)
 
-        if idx + 4 < len(registered_course_str_list) and not re.match(
-            r"^[A-Z]{2,4}\s[A-Z]\d{3}-[A-Z]\d", registered_course_str_list[idx + 4]
-        ):
+        if normal_class_match:
             parsed_course = Course(
                 registered_course_str_list[idx],
                 registered_course_str_list[idx + 1].split()[0],
-                registered_course_str_list[idx + 5].split()[1],
-                registered_course_str_list[idx + 4].split()[0],
-                registered_course_str_list[idx + 4].split()[1],
+                registered_course_str_list[idx + 3].split()[1],
+                registered_course_str_list[idx + 2].split()[0],
+                registered_course_str_list[idx + 2].split()[1],
             )
-
             parsed_courses.append(parsed_course)
 
-            registered_course_str_list.remove(registered_course_str_list[idx + 4])
-            registered_course_str_list.remove(registered_course_str_list[idx + 4])
+            if idx + 5 >= len(registered_course_str_list):
+                break
+
+            split_class_str_to_match = "\n".join(
+                [
+                    registered_course_str_list[idx],
+                    registered_course_str_list[idx + 1],
+                    registered_course_str_list[idx + 2],
+                    registered_course_str_list[idx + 3],
+                    registered_course_str_list[idx + 4],
+                    registered_course_str_list[idx + 5],
+                ]
+            )
+
+            split_class_match = re.match(split_class_pattern, split_class_str_to_match)
+
+            if split_class_match:
+                parsed_course = Course(
+                    registered_course_str_list[idx],
+                    registered_course_str_list[idx + 1].split()[0],
+                    registered_course_str_list[idx + 5].split()[1],
+                    registered_course_str_list[idx + 4].split()[0],
+                    registered_course_str_list[idx + 4].split()[1],
+                )
+                parsed_courses.append(parsed_course)
 
     return parsed_courses
